b8fae709985e4289a938e31cf78a0920cc5374a3
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-04-26 12:57:34 +0300

    add getRedirectResult

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
+  getRedirectResult,
 } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.6.11/firebase-auth.min.js";
 
 import { firebaseConfig } from "./firebaseConfig.js";
@@ -78,3 +79,24 @@ btn.addEventListener("click", () => {
       // ...
     });
 });
+
+getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+    console.log("getRedirectResult :>> ", getRedirectResult);
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
