af39a9895614d5857583581ee2c7f121e7885ac8
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-04-26 12:48:49 +0300

    first commit

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCPjk1E34nPDy4P83msEWktdV1uhHuk-Vg",
+  authDomain: "to-do-list-57e5e.firebaseapp.com",
+  databaseURL:
+    "https://to-do-list-57e5e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "to-do-list-57e5e",
+  storageBucket: "to-do-list-57e5e.appspot.com",
+  messagingSenderId: "638525864468",
+  appId: "1:638525864468:web:b14e6928d8a479105c4501",
+};
---
+++ js/script.js
@@ -0,0 +1,80 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { getDatabase } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.6.11/firebase-database.min.js";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.6.11/firebase-auth.min.js";
+
+import { firebaseConfig } from "./firebaseConfig.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth();
+
+//добавить данные
+function writeUserData(userId, name, email, imageUrl = "") {
+  push(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  });
+}
+
+function getData(userId) {
+  get(ref(db, "users/" + userId + "/-N0CgkHgtW1KdUNL41RO"))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+const a = {
+  email: "test@test.com",
+  profile_picture: "",
+  username: "Test 111111111",
+};
+
+function updateData(userId, updates) {
+  update(ref(db, "users/" + userId + "/-N0CgkHgtW1KdUNL41RO"), updates);
+}
+
+function removeData(userId) {
+  remove(ref(db, "users/" + userId + "/-N0CgkHgtW1KdUNL41RO"));
+}
+// writeUserData("shdbvjdasad", "Test", "test@test.com");
+// updateData("shdbvjdasad", a);
+// removeData("shdbvjdasad");
+// getData("shdbvjdasad");
+
+const btn = document.querySelector(".btn");
+const provider = new GoogleAuthProvider();
+
+btn.addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log("user :>> ", user);
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+});
