2d1e1b436d808d7c64c92ac5966cb4cacc8c69aa
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-04-26 13:08:57 +0300

    add getRedirectResult2

--- js/script.js
+++ js/script.js
@@ -80,24 +80,28 @@ btn.addEventListener("click", () => {
     });
 });
 
-getRedirectResult(auth)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access Google APIs.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
+getResultAuth(auth);
 
-    // The signed-in user info.
-    const user = result.user;
-    console.log("getRedirectResult :>> ", user);
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
-getRedirectResult(auth);
+function getResultAuth(auth) {
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      console.log("getRedirectResult :>> ", user);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log("credential :>> ", credential);
+      // ...
+    });
+}
